d8e4ae26b4bb47bf0b9380904489798d36a5f2e0
Margarita Kirichenko <rita.k.kirichenko@gmail.com>
2025-10-03 01:08:44 +0300

    Fix bounds

--- lr2/sum_before_and_after.c
+++ lr2/sum_before_and_after.c
@@ -1,21 +1,22 @@
 #include "sum_before_and_after.h"
 #include "index_last_zero.h"
 #include "index_first_zero.h"
+#include <stdlib.h>
 
 int sum_before_and_after(int values[], int length) {
     int sum = 0;
     int first = index_first_zero(values, length);
     int last = index_last_zero(values, length);
     
-    if (first > 0) {
+    if (first >= 0) {
         for(int n = 0; n < first; n++) {
-            sum += values[n];
+            sum += abs(values[n]);
         }
     }
 
-    if (last > 0) {
+    if (last >= 0) {
         for(int n = last + 1; n < length; n++) {
-            sum += values[n];
+            sum += abs(values[n]);
         }
     }
     return sum;
--- lr2/sum_between.c
+++ lr2/sum_between.c
@@ -1,15 +1,17 @@
 #include "sum_between.h"
 #include "index_last_zero.h"
 #include "index_first_zero.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int sum_between(int values[], int length) {
     int sum = 0;
     int first = index_first_zero(values, length);
     int last = index_last_zero(values, length);
 
-    if (first > 0 && last > 0) {
+    if (first >= 0 && last >= 0) {
         for(int n = first+1; n < last; n++){
-            sum += values[n];
+            sum += abs(values[n]);
         }
     }
     return sum;
